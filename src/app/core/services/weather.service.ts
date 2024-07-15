e82f3abd76edbe7b71b2fa8354cbded6cef0720c
Daniel Villera <davillera77@gmail.com>
2024-07-15 15:22:05 -0500

    initial commit

---
+++ src/app/core/services/weather.service.ts
@@ -0,0 +1,16 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  private http = inject(HttpClient);
+
+  getWeather(cityCode: string) {
+    return this.http.get(
+      `${environment.API_URL}/gridpoint/${cityCode}/31,80/forecast`
+    );
+  }
+}
